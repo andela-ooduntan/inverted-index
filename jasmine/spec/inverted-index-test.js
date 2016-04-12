0035e00324a784d16d87f4e9c9d385e5916123bd
Oduntan Stephen <stephen.oduntan@andela.com>
2016-04-12 07:34:11 +0100

    Passes all read json test

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -1,14 +1,36 @@
 //A test suit that "Read book data".
 //  -it checks 
-
 var indexObj= new invertedIndex();
-indexObj.createIndex('books.json');
+beforeEach(function (done){
+  indexObj.createIndex('books.json',done);
+  //done();
+});
+
 
 describe('Read book data', function () {
   // body...
   it('Reads the JSON file and asserts that it is not empty.',function () {
     // body...
-    expect(indexObj.isJsonEmpty).toBe(false);
+    expect(indexObj.isJsonEmpty()).toBe(false);
+  });
+  it('verifies that the title of the first object property in the array is of type string', function() {
+    expect(typeof indexObj.bookObject[0].title).toBe('string');
+  });
+  it('verifies that the text of the first object property in the array is of type string', function() {
+    expect(typeof indexObj.bookObject[0].text).toBe('string');
+  });
+  it('verifies that the title of the second object property in the array is of type string', function() {
+    expect(typeof indexObj.bookObject[1].title).toBe('string');
+  });
+  it('verifies that the text of the second object property in the array is of type string', function() {
+    expect(typeof indexObj.bookObject[1].text).toBe('string');
+  });
+
+  describe('Populate Index',function() {
+    // body...
+    it('Verifies the index is created once the file is read', function () {
+      expect(indexObj.getIndex()).toEqual([])
+    });
   });
   
 
--- src/inverted-index.js
+++ src/inverted-index.js
@@ -1,17 +1,11 @@
 var invertedIndex = function () {
     this.dictionary = {};
-    this.stopingWords = ['an','of','and',''];
+    this.stopingWords = ['an','of','and','in','the'];
     this.bookObject;
 }
 invertedIndex.prototype = {
-    createIndex : function (filepath) {
-    return this.readJSONfile(filepath,function (jsonContent) {
-      // body...
-      this.bookObject=jsonContent;
-      console.log(jsonContent);
-    });        
-    },
-    readJSONfile: function(filepath, callback) {
+    createIndex : function (filepath,done) {
+    //this.readJSONfile(filepath);
     var that = this;
     let xmlhttp;
     if (window.XMLHttpRequest) {
@@ -24,8 +18,8 @@ invertedIndex.prototype = {
     xmlhttp.onreadystatechange = function() {
       if (xmlhttp.readyState === 4) {
         if (xmlhttp.status === 200 ) {
-          that.jsonContent = JSON.parse(xmlhttp.responseText);
-          callback(that.jsonContent);
+          that.bookObject = JSON.parse(xmlhttp.responseText);
+          done()
         } else {
           console.error('An error has occured making the request');
           return false;
@@ -35,10 +29,28 @@ invertedIndex.prototype = {
 
     xmlhttp.open('GET', filepath, true);
     xmlhttp.setRequestHeader('Content-Type', 'application/json');
-    xmlhttp.send();
-  },
+    xmlhttp.send();        
+    },
   isJsonEmpty: function (){
-    console.log (this.bookObject);
+    //console.log (this.bookObject);
     return Object.keys(this.bookObject).length === 0;
-  }
+  },
+  getIndex : function () {
+    // body...
+    this.bookObject.forEach(function (value, index) {
+      // body...
+      var purifiedWords = this.purifyString(value);
+      purifiedWords[0].forEach(function () {
+        // body...
+        if (dictionary.hasOwnProperty) {}
+      });
+
+    });
+
+  },
+  purifyString : function (value) {
+    let textString = value.text.toLowerCase().replace(/(\.|\,|\:|\;|\-)/g, '').split(/\s/);
+    let titleString = value.title.toLowerCase().replace(/(\.|\,|\:|\;|\-)/g, '').split(/\s/);
+    return [titleString,textString];
+  } 
 }
