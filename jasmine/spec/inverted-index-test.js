eaa97dc8b575c63386a6e53cfd2288a6e4391a1b
Oduntan Stephen <stephen.oduntan@andela.com>
2016-04-15 15:32:06 +0100

    refactored code  and added populate function

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -1,9 +1,15 @@
 //A test suit that "Read book data".
 //  -it checks 
 var indexObj= new invertedIndex();
+var bookObject;
 beforeEach(function (done){
-  indexObj.createIndex('books.json',done);
+  indexObj.createIndex('books.json',function (parsedBookObject) {
+    // body...
+    bookObject = parsedBookObject;
+    done() 
+  });
   //done();
+  //console.log(indexObj.bookObject)
 });
 
 
@@ -11,38 +17,58 @@ describe('Read book data', function () {
   // body...
   it('Reads the JSON file and asserts that it is not empty.',function () {
     // body...
-    expect(indexObj.isJsonEmpty()).toBe(false);
+    //console.log(indexObj.stopingWords);
+    expect(indexObj.isJsonEmpty()).not.toBeTruthy();
   });
   it('verifies that the JSON file contains a JSON object', function() {
-    expect(typeof indexObj.bookObject).toBe('object');
+    expect(typeof bookObject).toBe('object');
   });
 
   it('verifies that the title of the first object property in the array is of type string', function() {
-    expect(typeof indexObj.bookObject[0].title).toBe('string');
+    expect(typeof bookObject[0].title).toBe('string');
   });
 
   it('verifies that the text of the first object property in the array is of type string', function() {
-    expect(typeof indexObj.bookObject[0].text).toBe('string');
+    expect(typeof bookObject[0].text).toBe('string');
   });
   it('verifies that the title of the second object property in the array is of type string', function() {
-    expect(typeof indexObj.bookObject[1].title).toBe('string');
+    expect(typeof bookObject[1].title).toBe('string');
   });
   it('verifies that the text of the second object property in the array is of type string', function() {
-    expect(typeof indexObj.bookObject[1].text).toBe('string');
+    expect(typeof bookObject[1].text).toBe('string');
   });
 
   describe('Populate Index',function() {
     // body...
     
     it('Verifies the index is created once the file is read', function () {
-      expect(Object.keys(indexObj.getIndex()).length).toBeGreaterThan(0);
+      //console.log(bookObject)
+      expect(Object.keys(indexObj.getIndex(bookObject)).length).toBeGreaterThan(0);
     });
 
 
     it('Verifies the index maps the string keys to the correct objects in the JSON array', function () {
-      expect(indexObj.dictionary.wonderland).toEqual([0]);
+      //indexObj.getIndex()
+      expect(indexObj.dictionary.wonderland).toEqual([[0,['TL',9]]]);
+    });
+
+    it('Verifies the index maps the string keys to the correct objects in the JSON array', function () {
+      expect(indexObj.dictionary.alice).toEqual([[0,['TL',0]],[0,['TX',0]]]);
+    });
+
+    it('Verifies the index maps the string keys to the correct objects in the JSON array', function () {
+      //console.log(indexObj);
+      expect(indexObj.dictionary.ring).toEqual([[1,['TL',44]],[1,['TX',82]]]);
     });
   });
   
+  describe('Search index',function () {
+    // body...
+    it('verifies that searching the index returns an array of the indices of the correct objects that contain the words in the search query',function () {
+      // body...
+      expect(indexObj.searchIndex('alice')).toEqual([ [ [ 0, [ 'TL', 0 ] ], [ 0, [ 'TX', 0 ] ] ] ] )
+
+    })
+  })
 
 });
--- src/inverted-index.js
+++ src/inverted-index.js
@@ -1,65 +1,128 @@
 var invertedIndex = function () {
     this.dictionary = {};
     this.stopingWords = ['an','of','and','in','the','a'];
-    this.bookObject;
 }
 invertedIndex.prototype = {
-    createIndex : function (filepath,done) {
-    //this.readJSONfile(filepath);
+    createIndex : function (filepath,callback) {
+      var parentProp=this;
+    this.readJSONfile(filepath,function (jsonData) {
+      // body...
+      parentProp.getIndex(jsonData);
+      
+      callback(jsonData);
+    });
+            
+    },
+    readJSONfile:function (filepath,callback) {
+      // body...
     var that = this;
     let xmlhttp;
     if (window.XMLHttpRequest) {
       xmlhttp = new XMLHttpRequest();
     } else {
       //for old browser
-      xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+      xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
     }
 
     xmlhttp.onreadystatechange = function() {
       if (xmlhttp.readyState === 4) {
         if (xmlhttp.status === 200 ) {
           that.bookObject = JSON.parse(xmlhttp.responseText);
-          done()
+          //done()
+          callback(that.bookObject);
         } else {
           console.error('An error has occured making the request');
           return false;
         }
       }
     };
-
+    //console.log(that.bookObject)
     xmlhttp.open('GET', filepath, true);
     xmlhttp.setRequestHeader('Content-Type', 'application/json');
-    xmlhttp.send();        
+    xmlhttp.send();
     },
   isJsonEmpty: function (){
     //console.log (this.bookObject);
     return Object.keys(this.bookObject).length === 0;
   },
-  getIndex : function () {
+  getIndex : function (bookObject) {
     // body...
-    let parentObj = this;
-    this.bookObject.forEach(function (value, index) {
+    var parentObj = this;
+    //console.log(bookObject)
+    bookObject.forEach(function (value, index) {
       // body...
-      var purifiedWords = parentObj.purifyString(value);
-      purifiedWords.forEach(function (titleString,arrayIndex) {
+      var purifiedTitle = parentObj.purifyString(value.title);
+      parentObj.populateDictionary(purifiedTitle,'TL', index);
+      var purifiedText = parentObj.purifyString(value.text);
+      parentObj.populateDictionary(purifiedText,'TX',index);
+
+    });
+    return parentObj.dictionary
+  },
+  populateDictionary: function (wordsArray,location,index) {
+    // body...
+    var parentObj = this;
+    var space = 0;
+    //console.log(wordsArray);
+          wordsArray.forEach(function (titleString,arrayIndex) {
         // body...
-       if (parentObj.dictionary.hasOwnProperty(titleString)) {
-        //let result = [index];
-        parentObj.dictionary[titleString].push(index);
+        if (arrayIndex !== 0 ) {
+          prevWordPos = arrayIndex-1;
+          space+=wordsArray[prevWordPos].length+1;
+        }else{
+          prevWordPos = false;
+        }
+
+        if (prevWordPos !== false) {
+          var result = [index,[location,space]];
+        }else{
+          var result = [index,[location,0]];
+        }
+        //console.log(result[1][0]);
+       if (parentObj.dictionary.hasOwnProperty(titleString)  && parentObj.checkIfWordExist(parentObj.dictionary[titleString],result)) {
+          parentObj.dictionary[titleString].push(result);
+        
         } else {
-          parentObj.dictionary[titleString] = [index];
+          parentObj.dictionary[titleString] = [result];
        //  //let result = [index]; && [index] === parentObj.dictionary[titleString]
        //  console.log(parentObj.dictionary[titleString]);
        //  parentObj.dictionary[titleString].push(index);
         }
       });
-
-    });
-    return parentObj.dictionary
   },
-  purifyString : function (value) {
-    var allWordString = value.text +' '+ value.title;
+  checkIfWordExist : function (array,searchTerm) {
+    // body...
+    var wordExisted=false;
+    for (var counter = 0; counter < array.length;  counter++) {
+      // body...
+      //console.log(searchTerm[1][0]);
+      //console.log(value[1][0]);
+      if ( array[counter].join() === searchTerm.join() ) {
+        wordExisted = true;
+        break;
+      }
+    }
+    if ( wordExisted ) {
+      return false;
+    } else {
+      return true;
+    }
+  },
+  purifyString : function (allWordString) {
     let textString = allWordString.toLowerCase().replace(/(\.|\,|\:|\;|\-)/g, '').split(/\s/);
     return textString;
+  },
+  searchIndex : function () {
+    // body...
+    var args = Object.keys(arguments).length;
+    var searchResult = []
+    for (var i = 0; i < arguments.length; i++) {
+      
+      if (this.dictionary[arguments[i]]) {
+        searchResult.push(this.dictionary[arguments[i]]);
+      }
+    }
+    //console.log()
+    return searchResult;
   } 
 }
