304ac78f6971eef469adda8d788cf53fb74c7760
Oduntan Stephen <stephen.oduntan@andela.com>
2016-04-18 23:26:34 +0100

    Did some refeactoring

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -1,140 +1,137 @@
-/* Creates an instace of invertedIndex*/
-var indexObj = new invertedIndex();
-/* Runs the createIndex before every spec. */
+// Creates an instace of invertedIndex
+var indexObj = new InvertedIndex();
+// Runs the createIndex before every spec. 
 beforeEach(function (done){
-  indexObj.createIndex('books.json',function (parsedBookObject) {
-    done();
-  });
+  indexObj.createIndex('books.json',done);
 });
 
 
 describe('Read book data', function () {
 
   it('Reads the JSON file and asserts that it is not empty.',function () {
-    /* This spec checks if the JSON object is empty. */
+    // This spec checks if the JSON object is empty. 
     expect( Object.keys(indexObj.bookObject).length ).toBeGreaterThan(0);
   });
+
   it('verifies that the JSON file contains a JSON object', function() {
-    /* This spec checks if the JSON object is an object. */
+    // This spec checks if the JSON object is an object. 
     expect(typeof indexObj.bookObject).toBe('object');
   });
 
   it('verifies that the title of the first object property in the array is of type string', function() {
-    /* This spec checks if the JSON object is an object. */
+    // This spec checks if the JSON object is an object. 
     expect(typeof indexObj.bookObject[0].title).toBe('string');
   });
 
   it('verifies that the text of the first object property in the array is of type string', function() {
-    /* This spec checks if the JSON object properties contain a string. */
+    // This spec checks if the JSON object properties contain a string. 
     expect(typeof indexObj.bookObject[0].text).toBe('string');
   });
+
   it('verifies that the title of the second object property in the array is of type string', function() {
-    /* This spec checks if the JSON object properties contain a string. */
+    // This spec checks if the JSON object properties contain a string. 
     expect(typeof indexObj.bookObject[1].title).toBe('string');
   });
+
   it('verifies that the text of the second object property in the array is of type string', function() {
-    /* This spec checks if the JSON object properties contain a string. */
+    // This spec checks if the JSON object properties contain a string. 
     expect(typeof indexObj.bookObject[1].text).toBe('string');
   });
+});
 
-  describe('Populate Index',function() {
+describe('Populate Index',function() {
 
-    it('Verifies the index is created once the file is read', function () {
-      /* Checks if the index have been created  */
-      expect(Object.keys(indexObj.getIndex(indexObj.bookObject)).length).toBeGreaterThan(0);
-    });
+  it('Verifies the index is created once the file is read', function () {
+    // Checks if the index have been created 
+    expect(Object.keys(indexObj.getIndex(indexObj.bookObject)).length).toBeGreaterThan(0);
+  });
 
 
-    it('Verifies the index maps the string keys to the correct objects in the JSON array', function () {
-      /* Checks if the index key maps to the correct object and position in the JSON array */
-      expect(indexObj.dictionary.wonderland).toEqual([[0,['TL',9]]]);
-    });
+  it('Verifies the index maps the string keys to the correct objects in the JSON array', function () {
+    // Checks if the index key maps to the correct object and position in the JSON array 
+    expect(indexObj.dictionary.wonderland).toEqual([[0,'TL',9]]);
+  });
 
-    it('Verifies the index maps the string keys to the correct objects in the JSON array', function () {
-      /* Checks if the index key maps to the correct object and position in the JSON array */
-      expect(indexObj.dictionary.alice).toEqual([
-        [0,['TL',0]],
-        [0,['TX',0]]
-        ]);
-    });
-
-    it('Verifies the index maps the string keys to the correct objects in the JSON array', function () {
-      /* Checks if the index key maps to the correct object and position in the JSON array */
-      expect(indexObj.dictionary.ring).toEqual([ 
-        [ 1, [ 'TL', 16 ] ],
-        [ 1, [ 'TL', 44 ] ], 
-        [ 1, [ 'TX', 82 ] ] 
+  it('Verifies the index maps the string keys to the correct objects in the JSON array', function () {
+    // Checks if the index key maps to the correct object and position in the JSON array 
+    expect(indexObj.dictionary.alice).toEqual([
+      [0,'TL',0],
+      [0,'TX',0]
       ]);
-    });
   });
+
+  it('Verifies the index maps the string keys to the correct objects in the JSON array', function () {
+    // Checks if the index key maps to the correct object and position in the JSON array 
+    expect(indexObj.dictionary.ring).toEqual([ 
+      [1,'TL',16],
+      [1,'TL',44], 
+      [1,'TX',82] 
+    ]);
+  });
+});
   
-  describe('Search index',function () {
+describe('Search index',function () {
 
-    it('verifies that searching the index returns an array of the indices of the correct objects that contain the words in the search query',function () {
-      /* Verifies if searching the index returns an array of the indices of the correct objects that contain the words in the search query. */
-      expect(indexObj.searchIndex('alice')).toEqual([
-          [ 
-            [ 0, [ 'TL', 0 ] ], 
-            [ 0, [ 'TX', 0 ] ] 
-          ]
-        ] );
-
-    });
-
-    it('verifies that searching the index returns an array empty array when searched with words not in the index',function () {
-      /* Verifies if searching the index returns an array of the indices of the correct objects that contain the words in the search query. */
-      expect( indexObj.searchIndex('segun')).toEqual([[]]);
-
-    });
-     it('Ensure searchIndex can handle a varied number of search terms as arguments.',function () {
-      /* Checks if search can handle varying number of arguments */
-      expect(indexObj.searchIndex('segun','tobi','wonderland')).toEqual([ 
-                  [  ], 
-                  [  ], 
-                  [ 
-                    [ 0, [ 'TL', 9 ] ] 
-                  ]
-                 ]);
-
-    });
-    it('Ensure searchIndex can handle a more complex search terms as arguments.',function () {
-      /* Checks if search can handle more complex arguments. */
-      expect(indexObj.searchIndex(['Rings','Yemi',['alice']],'yemi','stephen')).toEqual([ 
-          [
-             [ 1, [ 'TL', 16 ] ], 
-             [ 1, [ 'TL', 44 ] ], 
-             [ 1, [ 'TX', 82 ] ] 
-          ], 
-          [  ], 
-          [ 
-            [ 0, [ 'TL', 0 ] ], 
-            [ 0, [ 'TX', 0 ] ] 
-          ], 
-          [  ], 
-          [  ] 
-        ]  );
-
-    });
-
-        it('Ensure searchIndex can handle a sentence as arguments.',function () {
-          /* Checks if search can handle a sentences */
-      expect(indexObj.searchIndex( 'ALice in wonderland.','this is Home elf.' )).toEqual( [
-          [ 
-            [ 0, [ 'TL', 0 ] ],
-            [ 0, [ 'TX', 0 ] ] 
-          ], 
-          [ [ 0, [ 'TL', 6 ] ] ], 
-          [ [ 0, [ 'TL', 9 ] ] ], 
-          [  ], 
-          [  ], 
-          [  ], 
-          [ [ 1, [ 'TX', 27 ] ] ] 
-        ]  );
-
-    });
-
-
+  it('verifies that searching the index returns an array of the indices of the correct objects that contain the words in the search query',function () {
+    // Verifies if searching the index returns an array of the indices of the correct objects that contain the words in the search query. 
+    expect(indexObj.searchIndex('alice')).toEqual([
+        [ 
+          [0,'TL', 0], 
+          [0,'TX', 0] 
+        ]
+      ] );
 
   });
 
+  it('verifies that searching the index returns an array empty array when searched with words not in the index',function () {
+    // Verifies if searching the index returns an array of the indices of the correct objects that contain the words in the search query. 
+    expect( indexObj.searchIndex('checkpoint')).toEqual([[]]);
+
+  });
+
+  it('Ensure searchIndex can handle a varied number of search terms as arguments.',function () {
+    // Checks if search can handle varying number of arguments 
+    expect(indexObj.searchIndex('segun','tobi','wonderland')).toEqual([ 
+                [  ], 
+                [  ], 
+                [ 
+                  [0,'TL',9] 
+                ]
+               ]);
+
+  });
+
+  it('Ensure searchIndex can handle a more complex search terms as arguments.',function () {
+    // Checks if search can handle more complex arguments. 
+    expect(indexObj.searchIndex(['Rings','Yemi',['in','fall']],'yemi','stephen')).toEqual([ 
+        [ 
+          [ 1, 'TL', 16 ], 
+          [ 1, 'TL', 44 ], 
+          [ 1, 'TX', 82 ] 
+        ], 
+        [  ], 
+        [ [ 0, 'TL', 6 ] ], 
+        [ [ 0, 'TX', 6 ] ], 
+        [  ], 
+        [  ] 
+      ] );
+
+  });
+
+  it('Ensure searchIndex can handle a sentence as arguments.',function () {
+        // Checks if search can handle a sentences 
+    expect(indexObj.searchIndex( 'ALice in wonderland.','this is Home elf.' )).toEqual( [
+        [ 
+          [0,'TL',0],
+          [0,'TX', 0] 
+        ], 
+        [ [0,'TL', 6] ], 
+        [ [0,'TL', 9] ], 
+        [  ], 
+        [  ], 
+        [  ], 
+        [ [1,'TX', 27] ] 
+      ]  );
+
+  });
 });
--- src/inverted-index.js
+++ src/inverted-index.js
@@ -1,228 +1,230 @@
-(function () {
-   'use strict';
-}());
+'use strict';
 
 /**
  * The constructor of invertedInedx.
  */
-var invertedIndex = function () {
-    this.dictionary = {};
-    this.searchResult = [];
+var InvertedIndex = function () {
+  this.dictionary = {};
+  this.searchResult = [];
+  this.getSearchResult = this.getSearchResult.bind(this);
 };
-/* The properties of the invertedIndex object. */
-invertedIndex.prototype = {
-  // Creates the JSON object from the JSON file {filepath} and send the JSON object to the callback function
-    createIndex : function ( filepath,callback ) {
-      var parentProp = this;
-      // Call the function that read the JSON file asyn.
-    this.readJSONfile( filepath,function ( jsonData ) {
-      //Call the function that helps create the index of the file
-      parentProp.getIndex( jsonData );
-      callback( jsonData );
-    });
-            
-    },
-    // Reads the JSON file async.
-    readJSONfile : function ( filepath , callback ) {
-    var parentProp = this;
-    var xmlhttp;
-    // Checking if the browser have a built-in XMLHttpRequest object.
-    if ( window.XMLHttpRequest ) {
-      xmlhttp = new XMLHttpRequest();
+
+
+// Creates the JSON object from the JSON file {filepath} and 
+// sends the JSON object to the callback function. 
+InvertedIndex.prototype.createIndex = function ( filepath,callback ) {
+  var parentProp = this;
+  // Call the function that read the JSON file asyn. 
+  this.readJSONfile( filepath,function ( jsonData ) {
+    // Call the function that helps create the index of the file 
+    parentProp.getIndex( jsonData );
+    callback( );
+  });
+        
+};
+// Reads the JSON file async.
+InvertedIndex.prototype.readJSONfile  = function ( filepath , callback ) {
+  var parentProp = this;
+  var xmlhttp = null;
+  // Checking if the browser have a built-in XMLHttpRequest object.
+  if ( window.XMLHttpRequest ) {
+    xmlhttp = new XMLHttpRequest();
+  } else {
+    // For browser that don't support XMLHttpRequest 
+    xmlhttp = new window.ActiveXObject('Microsoft.XMLHTTP');
+  }
+
+  xmlhttp.onreadystatechange = function() {
+    if ( xmlhttp.readyState === 4 ) {
+      if (xmlhttp.status === 200 ) {
+        parentProp.bookObject = JSON.parse(xmlhttp.responseText);
+        callback(parentProp.bookObject);
+      } else {
+        console.error('An error has occured making the request');
+        return false;
+      }
+    }
+  };
+  xmlhttp.open('GET', filepath, true);
+  xmlhttp.setRequestHeader('Content-Type', 'application/json');
+  xmlhttp.send();
+};
+/**
+ * Creates an index of the sentence in the JSON object passed to it. 
+ * @param  {JSON Object} The document JSON object 
+ * @return {JSON Object} Then index of the document.
+ */
+InvertedIndex.prototype.getIndex = function ( bookObject ) {
+  var parentObj = this;
+
+  bookObject.forEach( function (value, index) {
+    // Analysises the document by tokenization and normalization. 
+    var purifiedTitle = parentObj.purifyString( value.title );
+    var purifiedText = parentObj.purifyString( value.text );
+
+    purifiedTitle = parentObj.splitSentence( purifiedTitle );
+    parentObj.populateDictionary( purifiedTitle,'TL', index );
+    purifiedText = parentObj.splitSentence( purifiedText );
+    parentObj.populateDictionary( purifiedText,'TX',index );
+
+  });
+
+  return parentObj.dictionary;
+};
+/**
+ * Add words to the dictionary object.
+ * @param  {Array} An array of words to be indexed.
+ * @param  {String} The location of the words in the doc. TX for text and TL for title.
+ * @param  {Int} The position of the words in the JSON object.
+ */
+InvertedIndex.prototype.populateDictionary = function ( wordsArray,location,index ) {
+  var parentObj = this;
+  parentObj.stringPosition = 0;
+
+  wordsArray.forEach( function (titleString,arrayIndex) {
+    // Stemmed each word by removing trailing 's'. 
+    titleString = parentObj.stemWord(titleString);
+    // Finds the location of the word in the doc. 
+    var wordIndex = parentObj.findWordIndex(arrayIndex,wordsArray,index,location);
+    var wordInfoInIndex = parentObj.dictionary[titleString];
+    // Checks if the word exist in the index. 
+    if ( parentObj.dictionary.hasOwnProperty(titleString) && parentObj.checkIfWordExist( wordInfoInIndex,wordIndex) ) {
+
+      parentObj.dictionary[titleString].push(wordIndex);
+
     } else {
-      // For browser that don't support XMLHttpRequest
-      xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
+
+      parentObj.dictionary[titleString] = [wordIndex];       
+    }
+  });
+};
+/**
+ * @param  {Int} The position of the word in the array after tokenization
+ * @param  {Array} The tokenized sentence.
+ * @param  {Int} The position of the sentence in the JSON object.
+ * @param  {String} The property location of the word in the JSON object. TX for text or TL for title.
+ * @return {Array} An array of the exact location of the word in the JSON object. 
+ * e.g [0,TX,12] '0' means the word is in the first JSON object and 
+ * 'TX' means the word is in the 'text' property and '12' means the word position in the doc. is 12.
+ */
+InvertedIndex.prototype.findWordIndex = function (positionInArray,wordsArray,positionInDoc,locationInDoc) {
+  var wordLocation = null;
+
+  if ( positionInArray !== 0 ) {
+    this.stringPosition += wordsArray[positionInArray-1].length+1;
+  }
+
+  if ( positionInArray-1 >= 0 ) {
+    wordLocation = [positionInDoc,locationInDoc,this.stringPosition];
+  }else{
+    wordLocation = [positionInDoc,locationInDoc,0];
+  }
+  return wordLocation;
+};
+/**
+ * Checks if a word exist in the index.
+ * @param  {Array} The location of previous words in the index
+ * @param  {Array} The location of the queried word.
+ * @return {Boolean} True if word exist and otherwise false.
+ */
+InvertedIndex.prototype.checkIfWordExist = function (array,searchTerm) {
+  var wordExists=false;
+
+  for (var counter = 0; counter < array.length;  counter++) {
+
+    if ( array[counter].join() === searchTerm.join() ) {
+      wordExists = true;
+      break;
     }
 
-    xmlhttp.onreadystatechange = function() {
-      if (xmlhttp.readyState === 4) {
-        if (xmlhttp.status === 200 ) {
-          parentProp.bookObject = JSON.parse(xmlhttp.responseText);
-          callback(parentProp.bookObject);
-        } else {
-          console.error('An error has occured making the request');
-          return false;
-        }
-      }
-    };
-    xmlhttp.open('GET', filepath, true);
-    xmlhttp.setRequestHeader('Content-Type', 'application/json');
-    xmlhttp.send();
-    },
-  /**
-   * Creates an index of the sentence in the JSON object passed to it. 
-   * @param  {JSON Object} The document JSON object 
-   * @return {JSON Object} Then index of the document.
-   */
-  getIndex : function (bookObject) {
+  }
 
-    var parentObj = this;
+  if ( wordExists ) {
+    return false;
+  } else {
+    return true;
+  }
+};
+/**
+ * Removes all alphanumeric character from a string and change to lower case (Normalization).
+ * @param  {String} The string to cleaned.
+ * @return {String} The cleaned string.
+ */
+InvertedIndex.prototype.purifyString = function ( allWordString ) {
+  var exp = /[,";:?!@#$%(^)&*()_+|.><{}±=-]/g;
+  allWordString = allWordString.toLowerCase();
+  allWordString = allWordString.replace(exp, '');
+  return allWordString;
+};
+/**
+ * Splits a sentence into an array (Tokenization)
+ * @param  {String}
+ * @return {Array}
+ */
+InvertedIndex.prototype.splitSentence = function ( sentence ) {
+  return sentence.split(/\s/);
+};
+/**
+ * Searches the index and pushes the result into searchResult property.
+ * @param  {Mixed} The argument can either be an array or a String.
+ */
+InvertedIndex.prototype.getSearchResult = function ( searchTerm ) {
+  var parentObj = this;
+  // Checks if the search term is an array. 
+  if ( searchTerm instanceof Array ) {
+    // Loops through an array of search term. 
+    searchTerm.forEach( function (value) {
+    // Search each element of an array of search term (Recursion). 
+    parentObj.getSearchResult(value);
+  }); 
 
-    bookObject.forEach( function (value, index) {
-      /* Analysises the document by tokenization and normalization. */
-      var purifiedTitle = parentObj.purifyString(value.title);
-      purifiedTitle = parentObj.splitSentence(purifiedTitle);
-      parentObj.populateDictionary(purifiedTitle,'TL', index);
-      var purifiedText = parentObj.purifyString(value.text);
-      purifiedText = parentObj.splitSentence(purifiedText);
-      parentObj.populateDictionary(purifiedText,'TX',index);
-
-    });
-
-    return parentObj.dictionary;
-  },
-  /**
-   * Add words to the dictionary object.
-   * @param  {Array} An array of words to be indexed.
-   * @param  {String} The location of the words in the doc. TX for text and TL for title.
-   * @param  {Int} The position of the words in the JSON object.
-   */
-  populateDictionary: function ( wordsArray,location,index ) {
-
-    var parentObj = this;
-    parentObj.stringPosition = 0;
-    
-
-      wordsArray.forEach( function (titleString,arrayIndex) {
-        /* Stemmed each word by removing trailing 's'. */ 
-        titleString = parentObj.stemWord(titleString);
-        /* Finds the location of the word in the doc. */
-        var wordIndex = parentObj.findWordIndex(arrayIndex,wordsArray,index,location);
-        /* Checks if the word exist in the index. */
-        if ( parentObj.dictionary.hasOwnProperty(titleString) && parentObj.checkIfWordExist(parentObj.dictionary[titleString],wordIndex) ) {
-
-          parentObj.dictionary[titleString].push(wordIndex);
-    
-        } else {
-
-          parentObj.dictionary[titleString] = [wordIndex];       
-        }
-      });
-  },
-  /**
-   * @param  {Int} The position of the word in the array after tokenization
-   * @param  {Array} The tokenized sentence.
-   * @param  {Int} The position of the sentence in the JSON object.
-   * @param  {String} The property location of the word in the JSON object. TX for text or TL for title.
-   * @return {Array} An array of the exact location of the word in the JSON object. 
-   * e.g [0, [TX,12]] '0' means the word is in the first JSON object and 
-   * 'TX' means the word is in the 'text' property and '12' means the word position in the doc. is 12.
-   */
-  findWordIndex : function (positionInArray,wordsArray,positionInDoc,locationInDoc) {
-    var wordLocation = null;
-    if ( positionInArray !== 0 ) {
-          this.stringPosition += wordsArray[positionInArray-1].length+1;
-        }
-        if ( positionInArray-1 >= 0 ) {
-           wordLocation = [positionInDoc,[locationInDoc,this.stringPosition]];
-        }else{
-           wordLocation = [positionInDoc,[locationInDoc,0]];
-        }
-        return wordLocation;
-  },
-  /**
-   * Checks if a word exist in the index.
-   * @param  {Array} The location of previous words in the index
-   * @param  {Array} The location of the queried word.
-   * @return {Boolean} True if word exist and otherwise false.
-   */
-  checkIfWordExist : function (array,searchTerm) {
-    var wordExisted=false;
-
-    for (var counter = 0; counter < array.length;  counter++) {
-      if ( array[counter].join() === searchTerm.join() ) {
-        wordExisted = true;
-        break;
-      }
-    }
-
-    if ( wordExisted ) {
-      return false;
+  }else {
+    // Search term is a String 
+    searchTerm = this.purifyString( searchTerm );
+    searchTerm = this.stemWord( searchTerm );
+    // Checks if search term is a sentence. 
+    if ( this.splitSentence(searchTerm).length > 1 ) {
+      // Searches each words in the sentence.
+      this.getSearchResult( this.splitSentence( searchTerm ) );
     } else {
-      return true;
-    }
-  },
-  /**
-   * Removes all alphanumeric character from a string and change to lower case (Normalization).
-   * @param  {String} The string to cleaned.
-   * @return {String} The cleaned string.
-   */
-  purifyString : function ( allWordString ) {
-    allWordString = allWordString.toLowerCase();
-    allWordString = allWordString.replace(/[,";:?!@#$%(^)&*()_+|.><{}±=-]/g, '');
-    return allWordString;
-  },
-  /**
-   * Splits a sentence into an array (Tokenization)
-   * @param  {String}
-   * @return {Array}
-   */
-  splitSentence : function ( sentence ) {
-    return sentence.split(/\s/);
-  },
-  /**
-   * Searches the index and pushes the result into searchResult property.
-   * @param  {Mixed} The argument can either be an array or a String.
-   */
-  getSearchResult : function ( searchTerm ) {
-    /* Checks if the search term is an array. */
-    if ( searchTerm instanceof Array ) {
-      /* Loops through an array of search term. */
-      searchTerm.forEach(function (value) {
-        /* Search each element of an array of search term (Recursion). */
-        parentObj.getSearchResult(value);
-      }); 
-
-    }else {
-      /* Search term is a String */
-      searchTerm = parentObj.purifyString(searchTerm);
-      searchTerm = parentObj.stemWord(searchTerm);
-      /* Checks if search term is a sentence. */
-      if (parentObj.splitSentence(searchTerm).length > 1) {
-        /* Searches each words in the sentence. */
-        parentObj.getSearchResult(parentObj.splitSentence(searchTerm));
+      // Search term is a word. 
+      if ( this.dictionary[searchTerm] ) {
+        this.searchResult.push(this.dictionary[searchTerm]);
       } else {
-        /* Search term is a word. */
-        if ( parentObj.dictionary[searchTerm] ) {
-          parentObj.searchResult.push(parentObj.dictionary[searchTerm]);
-        } else {
-          /* Pushes an empty array for words not in the index. */
-          parentObj.searchResult.push([]);
-        }
+        // Pushes an empty array for words not in the index.
+        this.searchResult.push([]);
       }
     }
-  },
-  /**
-   * Stems words to their root form. Works only with 'S' for now.
-   * @param  {String} Word with trailing 'S'.
-   * @return {String} Root form of word.
-   */
-  stemWord : function (word) {
-    return word.replace(/[s\z]/,'');
-  },
-  /**
-   * Searches for the index for a match with the search term.
-   * @return {Array} [Returns the location of the match found in an array]
-   */
-  searchIndex : function () {
-
-    parentObj = this;
-    this.searchResult = [];
-    
-    for (var i = 0; i < arguments.length; i++) {
-      /* Checks if the search term is an array. */
-      if ( arguments[i] instanceof Array ) {
-        arguments[i].forEach(this.getSearchResult);
-      } else {
-        /* Checks if the search term is a sentence. */
-        if ( this.splitSentence(arguments[i]).length > 1 ) {
-          this.getSearchResult(this.splitSentence(arguments[i]));
-      } else {
-         this.getSearchResult(this.purifyString(this.stemWord(arguments[i])));
-       }
-      }
+  }
+};
+/**
+ * Stems words to their root form. Works only with 'S' for now.
+ * @param  {String} Word with trailing 'S'.
+ * @return {String} Root form of word.
+ */
+InvertedIndex.prototype.stemWord = function (word) {
+  return word.replace(/[s\z]/,'');
+};
+/**
+ * Searches for the index for a match with the search term.
+ * @return {Array} [Returns the location of the match found in an array]
+ */
+InvertedIndex.prototype.searchIndex = function () {
+  this.searchResult = [];
+  
+  for (var i = 0; i < arguments.length; i++) {
+    // Checks if the search term is an array. 
+    if ( arguments[i] instanceof Array ) {
+      arguments[i].forEach(this.getSearchResult);
+    } else {
+      // Checks if the search term is a sentence. 
+      if ( this.splitSentence(arguments[i]).length > 1 ) {
+        this.getSearchResult(this.splitSentence(arguments[i]));
+    } else {
+       this.getSearchResult(this.purifyString(this.stemWord(arguments[i])));
+     }
     }
+  }
 
-    return this.searchResult;
-  } 
+  return this.searchResult;
 };
